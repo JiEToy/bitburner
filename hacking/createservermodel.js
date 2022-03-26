b7667be86cb0f32ca97544bc054d7fe3ffbeffc5
JiEToy <40161315+JiEToy@users.noreply.github.com>
2022-03-26 11:07:03 +0100

    Fix info for home server

--- hacking/createservermodel.js
+++ hacking/createservermodel.js
@@ -1,18 +1,18 @@
+/** Use this script to get the layout of all the servers in the current run. Results are stored as JSON in /servermodel.txt. */
+
 /** @param {NS} ns **/
 export async function main(ns) {
 	// Location is a server, connections are all servers directly connected to a location (found with scan(location)).
 	const allLocations = [];
-	const locationsIterator = ns.scan("home");
 	const scannedLocations = [];
-	// For home, add an object in allLocations.
-	allLocations.push({location:"home", connections:locationsIterator});
+	const locationsIterator = ns.scan("home");
+	// Add the object for home to allLocations.
+	const homeLocations = [...locationsIterator];
+	allLocations.push({location:"home", connections:homeLocations});
 	scannedLocations.push("home");
-	ns.tprint("allLocations: " + JSON.stringify(allLocations))
 	while (locationsIterator.length > 0) {
 		// For each server in the connectionList, scan it's connections.
 		const connectionsCurrentLocation = ns.scan(locationsIterator[0]);
-		ns.tprint(locationsIterator[0]);
-		ns.tprint("connectionsCurrentLocation: " + JSON.stringify(connectionsCurrentLocation));
 		// Put all locations that already have been scanned in a new list.
 		const newConnections = [];
 		for (let connection of connectionsCurrentLocation) {
@@ -20,18 +20,14 @@ export async function main(ns) {
 				newConnections.push(connection);
 			}
 		}
-		ns.tprint("newConnections: " + JSON.stringify(newConnections))
 		// Add this as an object in allLocations.
 		allLocations.push({location:locationsIterator[0], connections:newConnections});
-		// ns.tprint("allLocations: " + JSON.stringify(allLocations))
 		// Add all new connections to the list of locations to scan and add.
 		locationsIterator.push(...newConnections);
-		// ns.tprint("locationsIterator after concat: " + JSON.stringify(locationsIterator));
 		// Add scanned location to scannedLocations
 		scannedLocations.push(locationsIterator[0]);
 		locationsIterator.shift();
-		// ns.tprint("locationsIterator after shift: " + JSON.stringify(locationsIterator));
-		// ns.tprint("scannedLocations: " + JSON.stringify(scannedLocations));
 	}
-	await ns.write("test.txt", JSON.stringify(allLocations), 'w');
+	await ns.write("servermodel.txt", JSON.stringify(allLocations), 'w');
+	ns.tprint("Finished creating server model. Result stored in /servermodel.txt")
 }
