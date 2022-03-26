d6e96fc05f79d33693afd80fb8e043d387b86520
JiEToy <40161315+JiEToy@users.noreply.github.com>
2022-03-26 11:07:28 +0100

    Don't deploy on home

--- hacking/deployer.js
+++ hacking/deployer.js
@@ -1,3 +1,4 @@
+/** Run to hack all servers in current run. Make sure to run createservermodel.js first to create a list of all servers. */
 /** @param {NS} ns **/
 export async function main(ns) {
 	const servermodel = JSON.parse(ns.read("servermodel.txt"));
@@ -5,6 +6,8 @@ export async function main(ns) {
 	for (let server of servermodel) {
 		servers.push(server.location);
 	}
+	// Remove home from the list
+	servers.shift();
 	for (let serverName of servers) {
 		ns.run("/scripts/gainaccess.js", 1, serverName);
 		// Wait for the script to be done
