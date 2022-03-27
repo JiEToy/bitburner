ccc1e035c5aa3886034c6dace6ed36ce4f73ac4a
JiEToy <40161315+JiEToy@users.noreply.github.com>
2022-03-27 16:21:34 +0200

    Incorporate createservermodel.js in deployer

--- hacking/deployer.js
+++ hacking/deployer.js
@@ -1,6 +1,9 @@
 /** Run to hack all servers in current run. Make sure to run createservermodel.js first to create a list of all servers. */
 /** @param {NS} ns **/
 export async function main(ns) {
+	// We need to update the servermodel to be sure it is accurate.
+	ns.run("createservermodel.js");
+	await ns.sleep(1000);
 	const servermodel = JSON.parse(ns.read("servermodel.txt"));
 	const servers = [];
 	for (let server of servermodel) {
@@ -9,12 +12,12 @@ export async function main(ns) {
 	// Remove home from the list
 	servers.shift();
 	for (let serverName of servers) {
-		ns.run("/scripts/gainaccess.js", 1, serverName);
+		ns.run("gainaccess.js", 1, serverName);
 		// Wait for the script to be done
 		await ns.sleep(200);
 		// If we have successfully gained root, hack n00dles from this server.
 		if (ns.hasRootAccess(serverName)) {
-			ns.run("/scripts/hackn00dles.js", 1, serverName);
+			ns.run("hackjoesguns.js", 1, serverName);
 		}
 	}
 }
